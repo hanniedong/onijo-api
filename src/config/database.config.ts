48d0dd926f42bb637249c0efd4ec2bd75201ed96
Hannie Dong <hanniedong@Hannies-MacBook-Air-2.local>
2021-11-15 23:18:08 -0800

    Update config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,7 +3,7 @@ export const defaults = {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5445,
     username: process.env.DB_USERNAME || 'onijo',
-    password: process.env.DB_PASSWORD || 'password123',
+    password: process.env.DB_PASSWORD || 'onijo',
     database: process.env.DB_DATABASE || 'onijo_db',
     logging: process.env.DB_LOGGING || true,
     ssl: process.env.DB_USE_SSL === 'true' ? { rejectUnauthorized: false } : false,
