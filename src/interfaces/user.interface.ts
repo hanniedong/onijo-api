05db947636ded1c323423b943ab3b9362a42d272
Hannie Dong <hanniedong@Hannies-MacBook-Air-2.local>
2021-11-28 22:03:35 -0800

    Add phoneNumber

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -2,4 +2,5 @@ export interface UserInterface {
   email: string;
   username: string;
   id: string;
+  phoneNumber: string;
 }
--- src/mappers/user.mapper.ts
+++ src/mappers/user.mapper.ts
@@ -2,12 +2,13 @@ import { UserEntity } from '@users/entitites/user.entity';
 import { UserDto } from '../users/dto/user.dto';
 
 export const toUserDto = (data: UserEntity): UserDto => {
-  const { id, username, email } = data;
+  const { id, username, email, phoneNumber } = data;
 
   let userDto: UserDto = {
     id,
     username,
     email,
+    phoneNumber
   };
 
   return userDto;
