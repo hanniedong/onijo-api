966e9b4019f8e0a2b4cff5b2490791fb16032535
Hannie Dong <hanniedong@Hannies-MacBook-Air-2.local>
2022-01-20 11:50:50 -0800

    Change synchronize

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -7,7 +7,7 @@ export const defaults = {
   database: process.env.DB_DATABASE || 'onijo_db',
   logging: process.env.DB_LOGGING || true,
   ssl: process.env.DB_USE_SSL === 'true' ? { rejectUnauthorized: false } : false,
-  synchronize: true,
+  synchronize: false,
   seeds: [__dirname + ' /../database/seeds/**/*.seed.ts'],
   factories: [__dirname + '/../database/factories/**/*.factory.ts'],
   entities: [__dirname + '/../database/entities/*.entity{.ts,.js}'],
